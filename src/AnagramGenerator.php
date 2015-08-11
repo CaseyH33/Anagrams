e3c6d934079ee48599aca5a029837b3852be27e0
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-08-11 11:45:01 -0700

    Change anagramCheck to sort letters and compare sorted arrays

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -3,7 +3,15 @@
     {
         function anagramCheck($input_word, $input_wordsToCheck)
         {
-            if($input_word == $input_wordsToCheck)
+            //$array_of_words = explode(" ", $input_wordsToCheck);
+            $letter_array = str_split($input_word);
+            sort($letter_array);
+            $wordToCheck_letter_array = str_split($input_wordsToCheck);
+            sort($wordToCheck_letter_array);
+            //$matched_words = array();
+
+
+            if($letter_array == $wordToCheck_letter_array)
             {
                 return $input_wordsToCheck;
             } else {
