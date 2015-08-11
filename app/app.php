19b4cfc2c1404eac1e5fb2013fcbe32907ce7022
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-08-11 14:09:13 -0700

    Fix route error in app.php

--- app/app.php
+++ app/app.php
@@ -5,7 +5,7 @@
     $app = new Silex\Application();
     $app['debug'] = true;
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'../views'));
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('input_form.html.twig');
