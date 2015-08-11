15de5f17cd6991a79428d0b7641b22b05bc2baed
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-08-11 11:00:39 -0700

    Create and fail second test.

--- tests/AnagramGeneratorTest.php
+++ tests/AnagramGeneratorTest.php
@@ -17,5 +17,19 @@
             //Assert
             $this->assertEquals($input_wordsToCheck, $result);
         }
+
+        function test_letter_check_false()
+        {
+            //Arrange
+            $test_anagram = new AnagramGenerator;
+            $input_word = "a";
+            $input_wordsToCheck = "b";
+
+            //Act
+            $result = $test_anagram->anagramCheck($input_word, $input_wordsToCheck);
+
+            //Assert
+            $this->assertEquals("No matches found.", $result);
+        }
     }
 ?>
