c301b82e15f981e736b0fdee99de63e7518adb67
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-08-11 12:13:28 -0700

    change function to search multi-word string for anagram matches

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -3,19 +3,24 @@
     {
         function anagramCheck($input_word, $input_wordsToCheck)
         {
-            //$array_of_words = explode(" ", $input_wordsToCheck);
+            $array_of_words = explode(" ", $input_wordsToCheck);
             $letter_array = str_split($input_word);
             sort($letter_array);
-            $wordToCheck_letter_array = str_split($input_wordsToCheck);
-            sort($wordToCheck_letter_array);
-            //$matched_words = array();
+            $matched_words = array();
 
+            foreach($array_of_words as $word) {
+                $sorted_word = str_split($word);
+                sort($sorted_word);
+                if($sorted_word == $letter_array)
+                {
+                    array_push($matched_words, $word);
+                }
+            }
 
-            if($letter_array == $wordToCheck_letter_array)
-            {
-                return $input_wordsToCheck;
-            } else {
+            if(empty($matched_words)) {
                 return "No matches found.";
+            } else {
+                return implode(" ", $matched_words);
             }
         }
     }
