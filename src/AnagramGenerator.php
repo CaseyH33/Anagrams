3dd5bffb8083cec712977263e5a7c0f5beddd0a1
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-08-11 10:56:26 -0700

    Create a successful function to pass first test

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -3,7 +3,10 @@
     {
         function anagramCheck($input_word, $input_wordsToCheck)
         {
-
+            if($input_word == $input_wordsToCheck)
+            {
+                return $input_wordsToCheck;
+            }
         }
     }
 ?>
